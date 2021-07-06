5bfc1f7cd0c1cead6cf08b25e546b60ef2d9e165
Garry Kuwanto <gkuwanto@gmail.com>
2021-07-06 11:15:24 +0700

    implement calculator

--- main.go
+++ main.go
@@ -3,15 +3,40 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"math"
 )
 
 func calculatorHandler(w http.ResponseWriter, req *http.Request) {
 
-	numA := req.URL.Query().Get("num_a")
-	numB := req.URL.Query().Get("num_b")
+	numA, err := strconv.Atoi(req.URL.Query().Get("num_a"))
+	numB, err := strconv.Atoi(req.URL.Query().Get("num_b"))
+	if err != nil {
+		fmt.Fprintf(w,"Invalid number")
+		return
+	}
 	operator := req.URL.Query().Get("operator")
+	var result int
+	switch operator {
+	case "plus":
+		result = numA + numB	
+	case "minus":
+		result = numA - numB
+	case "times":
+		result = numA * numB
+	case "divide":
+		result = numA / numB
+	case "power":
+		resultf := math.Pow(float64(numA),float64(numB))
+		fmt.Fprintf(w, "%v\n", resultf)
+		return
+	default:
+		fmt.Fprintf(w, "invalid operator")	
+		return
+	}
 
-	fmt.Fprintf(w, fmt.Sprint(numA, numB, operator))
+
+	fmt.Fprintf(w,"%v\n",result) 
 }
 
 func main() {
